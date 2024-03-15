a21c0b4ae26e7fa18664e2027c3cc1d2e1902fa8
areskill <areskul@areskul.com>
2024-03-15 11:20:10 +0100

    test(nur): doc import

--- llm/default.nix
+++ llm/default.nix
@@ -29,7 +29,7 @@ in {
     freeglut
     # Python dependencies managment
     poetry
-    config.nur.repos.dustinblackman.oatmeal
+    nur.repos.dustinblackman.oatmeal
   ];
 
   systemd.services."ollama" = {
