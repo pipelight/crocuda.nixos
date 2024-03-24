667817492c8c704dd1db6cfadbf562880c4d9fc9
areskill <areskul@areskul.com>
2024-03-24 17:03:14 +0100

    add hyprland utils

--- wm/hyprland/default.nix
+++ wm/hyprland/default.nix
@@ -6,7 +6,7 @@
   inputs,
   ...
 }: let
-cfg = config.crocuda;
+  cfg = config.crocuda;
   convert_to_grayscale = pkgs.writeShellScriptBin "convert_to_grayscale" ''
     convert $1 -colorspace gray $1.gray.jpeg
   '';
@@ -27,6 +27,7 @@ in {
   environment.systemPackages = with pkgs; [
     wl-clipboard
     hyprlock
+    hypridle
     # Screen light
     redshift
     feh
