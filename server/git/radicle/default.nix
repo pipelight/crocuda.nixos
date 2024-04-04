b74125e0809660bbc408201184e19ffb2de3ecb9
areskill <areskul@areskul.com>
2024-04-04 11:31:35 +0200

    add radicle

--- server/git/radicle/default.nix
+++ server/git/radicle/default.nix
@@ -1,38 +1,41 @@
 {
   config,
   pkgs,
+  lib,
   inputs,
   ...
 }: let
   cfg = config.crocuda;
   username = "radicle";
-in {
-  users.users."${username}" = {
-    isNormalUser = true;
-  };
+in
+  with lib;
+    mkIf cfg.wm.hyprland.enable {
+      users.users."${username}" = {
+        isNormalUser = true;
+      };
 
-  environment.systemPackages = with pkgs; [
-    # Decentralized code collaboration plateform
-    # radicle-cli
-    inputs.radicle.packages.${system}.default
-    inputs.radicle-tui.packages.${system}.default
-  ];
+      environment.systemPackages = with pkgs; [
+        # Decentralized code collaboration plateform
+        # radicle-cli
+        inputs.radicle.packages.${system}.default
+        inputs.radicle-tui.packages.${system}.default
+      ];
 
-  systemd.services."radicle-node" = {
-    enable = true;
-    description = "Radicle node daemon";
-    documentation = ["https://radicle.xyz/guides/user"];
-    requires = ["network-online.target"];
-    serviceConfig = {
-      ExecStart = "${pkgs.charm}/bin/rad node start";
-      User = "${username}";
-      Group = "users";
-      Type = "simple";
-      Restart = "always";
-      RestartSec = 1;
-      WorkingDirectory = "/home/${username}/sandbox";
-      # EnvironmentFile = ["/etc/charm.conf"];
-    };
-    wantedBy = ["multi-user.target"];
-  };
-}
+      systemd.services."radicle-node" = {
+        enable = true;
+        description = "Radicle node daemon";
+        documentation = ["https://radicle.xyz/guides/user"];
+        requires = ["network-online.target"];
+        serviceConfig = {
+          ExecStart = "${pkgs.charm}/bin/rad node start";
+          User = "${username}";
+          Group = "users";
+          Type = "simple";
+          Restart = "always";
+          RestartSec = 1;
+          WorkingDirectory = "/home/${username}";
+          # EnvironmentFile = ["/etc/charm.conf"];
+        };
+        wantedBy = ["multi-user.target"];
+      };
+    }
