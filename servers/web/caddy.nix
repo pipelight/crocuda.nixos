35b1c68e3b77910feb1ee32ba7680dab521bb441
areskill <areskul@areskul.com>
2024-04-06 13:14:43 +0200

    run caddy as normal user

--- servers/web/caddy.nix
+++ servers/web/caddy.nix
@@ -6,17 +6,28 @@
   ...
 }: let
   cfg = config.crocuda;
+  # A dedicated unpriviledged user for public node
+  username = "caddy";
 in
   with lib;
-    mkIf cfg.servers.web.caddy.enable {
+    mkIf cfg.servers.web.enable {
+
+      users.users."${username}" = {
+        isNormalUser = true;
+      };
+
       environment.defaultPackages = with pkgs; [
-        # Crocuda dependencies
         caddy
       ];
+
       systemd.services.caddy = {
         enable = true;
         serviceConfig = {
           ExecStart = "${pkgs.caddy}/bin/caddy run";
+          User = "${username}";
+          Group = "users";
+          Restart = "always";
+          RestartSec = 3;
         };
       };
     }
