27b739b0c99cde1378b3fea5c8f5ca4773345093
areskill <areskul@areskul.com>
2024-04-08 17:07:09 +0200

    conditional import

--- base/yubikey.nix
+++ base/yubikey.nix
@@ -6,6 +6,9 @@
 }: {
   services.udev.packages = [pkgs.yubikey-personalization];
 
+  environment.systemPackages = with pkgs; [
+  yubikey-manager
+    ];
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
