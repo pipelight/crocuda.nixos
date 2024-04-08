5491ba7e59e413931f45776f406665f5634ec4d2
areskill <areskul@areskul.com>
2024-04-08 19:43:04 +0200

    add rule

--- base/yubikey.nix
+++ base/yubikey.nix
@@ -7,15 +7,24 @@
   services.udev.packages = [pkgs.yubikey-personalization];
 
   environment.systemPackages = with pkgs; [
-  yubikey-manager
-    ];
+    yubikey-manager
+  ];
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   #};
-  
+
   security.pam.services = {
     login.u2fAuth = true;
     sudo.u2fAuth = true;
   };
+  services.udev.extraRules = ''
+     ACTION=="remove",\
+     ENV{ID_BUS}=="usb",\
+     ENV{ID_MODEL_ID}=="0407",\
+     ENV{ID_VENDOR_ID}=="1050",\
+     ENV{ID_VENDOR}=="Yubico",\
+     RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+     RUN+="ps aux | egrep '(tty|pts)' | awk '{print $2}' | xargs kill -KILL"
+  '';
 }
