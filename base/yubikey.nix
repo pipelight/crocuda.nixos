1e3796a68e935a56298beec42fa98c3f287f0e4c
areskill <areskul@areskul.com>
2024-04-09 13:42:42 +0200

    udev exec shell script

--- base/yubikey.nix
+++ base/yubikey.nix
@@ -3,7 +3,12 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  kill_all_sessions = pkgs.writeShellScriptBin "kill_all_sessions" ''
+    ps aux | egrep '(tty|pts)' | awk '{print $2}' | xargs kill -KILL
+
+  '';
+in {
   services.udev.packages = [pkgs.yubikey-personalization];
 
   environment.systemPackages = with pkgs; [
@@ -19,12 +24,12 @@
     sudo.u2fAuth = true;
   };
   services.udev.extraRules = ''
-     ACTION=="remove",\
-     ENV{ID_BUS}=="usb",\
-     ENV{ID_MODEL_ID}=="0407",\
-     ENV{ID_VENDOR_ID}=="1050",\
-     ENV{ID_VENDOR}=="Yubico",\
-     RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-     RUN+="ps aux | egrep '(tty|pts)' | awk '{print $2}' | xargs kill -KILL"
+    ACTION=="remove",\
+    ENV{ID_BUS}=="usb",\
+    ENV{ID_MODEL_ID}=="0407",\
+    ENV{ID_VENDOR_ID}=="1050",\
+    ENV{ID_VENDOR}=="Yubico",\
+    RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+    RUN+="kill_all_sessions"
   '';
 }
