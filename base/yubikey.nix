761757442ed514284b0792a47042ca900b189d18
areskill <areskul@areskul.com>
2024-04-09 16:12:56 +0200

    try w/ systemd

--- base/yubikey.nix
+++ base/yubikey.nix
@@ -32,12 +32,19 @@ in {
     gnugrep
   ];
 
+  systemd.services."kill_all_sessions" = {
+    enable = true;
+    description = "Kill all sessions";
+    serviceConfig = {
+      ExecStart = "${kill_all_sessions}";
+    };
+  };
   services.udev.extraRules = ''
     ACTION=="remove",\
-      ENV{ID_BUS}=="usb",\
-      ENV{ID_MODEL_ID}=="0407",\
-      ENV{ID_VENDOR_ID}=="1050",\
-      ENV{ID_VENDOR}=="Yubico",\
-      RUN+="${pkgs.procps}/bin/ps aux | egrep '(tty|pts)' | xargs kill -KILL"
+    ENV{ID_BUS}=="usb",\
+    ENV{ID_MODEL_ID}=="0407",\
+    ENV{ID_VENDOR_ID}=="1050",\
+    ENV{ID_VENDOR}=="Yubico",\
+    RUN+="${pkgs.systemd}/bin/systemctl start kill_all_sessions"
   '';
 }
