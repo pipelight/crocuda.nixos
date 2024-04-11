c90a04a14003e63a6e91d3e0763e71b95a222d8c
areskill <areskul@areskul.com>
2024-04-11 13:13:02 +0200

    fix paths

--- terminal/kitty.nix
+++ terminal/kitty.nix
@@ -18,7 +18,7 @@
       set -x
 
       OUTDIR="$HOME/.config.deref/"
-      CONFIG_PATHS=".config/kitty .config/nvim .aliases"
+      CONFIG_PATHS=".config/fish .config/nvim .aliases"
 
       # Ensure output path
       mkdir -p $OUTDIR
