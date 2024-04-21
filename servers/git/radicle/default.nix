bd963f5a8de2f9c4055b35b8f57ad1235822ba89
areskill <areskul@areskul.com>
2024-04-21 15:33:43 +0200

    test

--- servers/git/radicle/default.nix
+++ servers/git/radicle/default.nix
@@ -19,7 +19,7 @@ in
         # Decentralized code collaboration plateform
         # radicle-cli
         # inputs.radicle.packages.${system}.default
-        inputs.radicle.packages.${system}.radicle-full
+        inputs.radicle.packages.${system}.rad
         inputs.radicle-interface.packages.${system}.default.override
         {
           doCheck = false;
@@ -42,7 +42,7 @@ in
         documentation = ["https://radicle.xyz/guides/user"];
         requires = ["network-online.target"];
         serviceConfig = with pkgs; let
-          package = inputs.radicle.packages.${system}.radicle-full;
+          package = inputs.radicle.packages.${system}.rad;
         in {
           ExecStart = "${package}/bin/radicle-node --listen 0.0.0.0:8776 --force";
           User = "${username}";
@@ -62,7 +62,7 @@ in
         documentation = ["https://radicle.xyz/guides/user"];
         requires = ["network-online.target"];
         serviceConfig = with pkgs; let
-          package = inputs.radicle.packages.${system}.radicle-full;
+          package = inputs.radicle.packages.${system}.rad;
         in {
           ExecStart = "${package}/bin/radicle-httpd --listen 127.0.0.1:8786 --force";
           User = "${username}";
