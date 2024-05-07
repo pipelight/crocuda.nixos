f9e9e017af9e0cd9c09b9178c488ca1df3f86606
areskill <areskul@areskul.com>
2024-05-07 18:32:25 +0200

    add option

--- servers/social/default.nix
+++ servers/social/default.nix
@@ -6,16 +6,18 @@
   ...
 }: let
   cfg = config.crocuda;
-in {
-  # security.acme = {
-  #   acceptTerms = true;
-  #   defaults.email = "<EMAIL TO USE FOR CORRESPONDENCE WITH Let's Encrypt>";
-  # };
-  services.mastodon = {
-    enable = true;
-    localDomain = "social.example.com"; # Replace with your own domain
-    # configureNginx = true;
-    smtp.fromAddress = "noreply@social.example.com"; # Email address used by Mastodon to send emails, replace with your own
-    extraConfig.SINGLE_USER_MODE = "true";
-  };
-}
+in
+  with lib;
+    mkIf cfg.servers.social.mastodon.enable {
+      # security.acme = {
+      #   acceptTerms = true;
+      #   defaults.email = "<EMAIL TO USE FOR CORRESPONDENCE WITH Let's Encrypt>";
+      # };
+      services.mastodon = {
+        enable = true;
+        localDomain = "social.example.com"; # Replace with your own domain
+        # configureNginx = true;
+        smtp.fromAddress = "noreply@social.example.com"; # Email address used by Mastodon to send emails, replace with your own
+        extraConfig.SINGLE_USER_MODE = "true";
+      };
+    }
