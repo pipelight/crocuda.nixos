47c9e6db2844771eceac61faa27f4840e381939d
areskill <areskul@areskul.com>
2024-05-13 17:14:40 +0200

    rebuild obs studio

--- stream/default.nix
+++ stream/default.nix
@@ -9,10 +9,25 @@
 in
   with lib;
     mkIf cfg.stream.enable {
+      # https://nixos.wiki/wiki/OBS_Studio
+      boot.extraModulePackages = with config.boot.kernelPackages; [
+        v4l2loopback
+      ];
+      boot.extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+      '';
+      security.polkit.enable = true;
+
       environment.systemPackages = with pkgs; [
-        obs-studio
-        obs-studio-plugins.wlrobs
-        obs-studio-plugins.input-overlay
         obs-cli
       ];
+      # Import home files
+      home-merger = {
+        enable = true;
+        extraSpecialArgs = {inherit cfg pkgs inputs;};
+        users = cfg.users;
+        modules = [
+          ./home.nix
+        ];
+      };
     }
---
+++ stream/home.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  cfg,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+       obs-backgroundremoval
+      obs-pipewire-audio-capture
+
+      input-overlay
+    ];
+  };
+}
