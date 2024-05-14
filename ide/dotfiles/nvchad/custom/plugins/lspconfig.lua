a79f39fa82ab2e9b063ec4714b1598097316eda6
areskill <areskul@areskul.com>
2024-05-14 14:53:19 +0200

    revert bad import

--- ide/dotfiles/nvchad/custom/plugins/lspconfig.lua
+++ ide/dotfiles/nvchad/custom/plugins/lspconfig.lua
@@ -1,4 +1,4 @@
-local configs = require("nvchad.configs.lspconfig")
+local configs = require("plugins.configs.lspconfig")
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
