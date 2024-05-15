0b08e9b3e11c874def0805605b7adf8299cdaab6
areskill <areskul@areskul.com>
2024-05-15 20:25:18 +0200

    add pebble challenge server

--- servers/web/test.nix
+++ servers/web/test.nix
@@ -6,8 +6,21 @@
   ...
 }: let
   cfg = config.crocuda;
+  pebbleConfig = pkgs.writeText "pebble.json" (builtins.toJSON {
+    pebble = {
+      listenAddress = "127.0.0.1:14000";
+      managementListenAddress = "127.0.0.1:15000";
+      certificate = "${pkgs.pebble.src}/test/certs/localhost/cert.pem";
+      privateKey = "${pkgs.pebble.src}/test/certs/localhost/key.pem";
+      httpPort = 5002;
+      tlsPort = 5001;
+      ocspResponderURL = "";
+      externalAccountBindingRequired = false;
+    };
+  });
 in {
   environment.defaultPackages = with pkgs; [
+    # https://github.com/letsencrypt/pebble
     pebble
   ];
 
@@ -20,20 +33,17 @@ in {
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
 
-  systemd.services.pebble = let
-    pebbleConfig = pkgs.writeText "pebble.json" (builtins.toJSON {
-      pebble = {
-        listenAddress = "127.0.0.1:14000";
-        managementListenAddress = "127.0.0.1:15000";
-        certificate = "${pkgs.pebble.src}/test/certs/localhost/cert.pem";
-        privateKey = "${pkgs.pebble.src}/test/certs/localhost/key.pem";
-        httpPort = 5002;
-        tlsPort = 5001;
-        ocspResponderURL = "";
-        externalAccountBindingRequired = false;
-      };
-    });
-  in {
+  systemd.services.pebble-challtestsrv = {
+    description = "Pebble ACME Challenge Test Server";
+    after = ["network-online.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Environment = ["PEBBLE_VA_NOSLEEP=1" "PEBBLE_VA_ALWAYS_VALID=1"];
+      ExecStart = "${pkgs.pebble}/bin/pebble-challtestsrv";
+      DynamicUser = true;
+    };
+  };
+  systemd.services.pebble = {
     description = "Pebble ACME Test Server";
     after = ["network-online.target"];
     wantedBy = ["multi-user.target"];
