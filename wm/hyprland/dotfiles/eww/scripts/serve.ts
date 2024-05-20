4c3775205c81bc612cadccf405b210cd72e2c8fd
areskill <areskul@areskul.com>
2024-05-20 17:33:04 +0200

    update to deno2

--- wm/hyprland/dotfiles/eww/scripts/serve.ts
+++ wm/hyprland/dotfiles/eww/scripts/serve.ts
@@ -1,12 +1,14 @@
 export const serve = async (fn: any) => {
   const hypr_instance = Deno.env.get("HYPRLAND_INSTANCE_SIGNATURE");
 
+  const runtime_dir = Deno.env.get("XDG_RUNTIME_DIR");
+
   let connection = await Deno.connect({
     transport: "unix",
-    path: `/tmp/hypr/${hypr_instance}/.socket2.sock`,
+    path: `${runtime_dir}/hypr/${hypr_instance}/.socket2.sock`,
   });
 
-  for await (const chunk of Deno.iter(connection)) {
+  for await (const chunk of connection.readable.values()) {
     // Read socket event
     let str = new TextDecoder().decode(chunk);
     // Exec provided function
