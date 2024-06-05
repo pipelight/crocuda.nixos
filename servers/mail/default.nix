e11459e33a579082911ecca22bc8c43f732aafb9
areskill <areskul@areskul.com>
2024-06-05 14:25:15 +0200

    add jucenit proxy to maddy

--- servers/mail/default.nix
+++ servers/mail/default.nix
@@ -12,6 +12,14 @@
 in
   with lib;
     mkIf cfg.mail.maddy.enable {
+      environment.etc = {
+        "jucenit/juceni.maddy.toml".source = ./dotfiles/jucenit.maddy.toml;
+      };
+      systemd.services.maddy.serviceConfig = {
+        ExecStart = ''
+          jucenit push --file /etc/jucenit/jucenit.maddy.toml
+        '';
+      };
       # The mail server
       services.maddy = {
         enable = true;
@@ -40,26 +48,5 @@ in
             port = 587;
           };
         };
-      };
-
-      environment.etc = {
-        "caddy/Maddy.Caddyfile".source = ./dotfiles/Maddy.Caddyfile;
-      };
-
-      systemd.services."caddy_expose_maddy" = {
-        enable = false;
-        after = ["caddy.service"];
-        description = "Load a caddy proxy config to redirect tls to maddy mail server";
-        serviceConfig = {
-          User = "caddy";
-          Group = "users";
-          WorkingDirectory = "~";
-          ExecStart = ''
-            ${pkgs.caddy}/bin/caddy reload \
-              --adapter caddyfile \
-              --config /etc/caddy/Caddyfile
-          '';
-        };
-        wantedBy = ["multi-user.target"];
       };
     }
