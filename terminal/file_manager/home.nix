55cd991d3e3718ec8c77ca26adfe44436fdcc276
areskill <areskul@areskul.com>
2024-12-04 12:00:39 +0100

    update yazi conf

--- terminal/file_manager/home.nix
+++ terminal/file_manager/home.nix
@@ -10,6 +10,7 @@ with lib;
   mkIf cfg.terminal.file_manager.enable {
     home.file = {
       ".config/yazi/keymap.toml".source = dotfiles/yazi/keymap.toml;
+      ".config/yazi/yazi.toml".source = dotfiles/yazi/yazi.toml;
     };
 
     home.packages = with pkgs; [
