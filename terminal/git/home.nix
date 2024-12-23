30e43daf2e7386d0ecfd13290de95badbc0b9557
areskill <areskul@areskul.com>
2024-12-23 12:12:40 +0100

    fix(file): add git to filetree

--- terminal/git/home.nix
+++ terminal/git/home.nix
@@ -27,22 +27,31 @@ with lib;
       jujutsu = {
         enable = true;
         settings = {
+          git = {
+            auto-local-bookmark = true;
+          };
           ui = {
             editor = "nvim -u ~/.config/nvchad/init.lua";
             # pager = "nvim -u ~/.config/nvim/init.lua";
             pager = "less";
           };
+          # signing = {
+          #   sign-all = true;
+          #   bakend = "ssh";
+          #   key = "~/.ssh/id_ed25519_signing";
+          #   backends.ssh.allowed-signers = "~/.ssh/allowed-signers";
+          # };
           templates = {
             draft_commit_descritption = let
               hint = builtins.replaceStrings ["#"] ["JJ:"] (builtins.readFile ./dotfiles/conventional_commit_message);
             in ''
               concat(
                 description,
+                "\n${hint}",
                 surround(
                   "\nJJ: This commit contains the following changes:\n", "",
                   indent("JJ:     ", diff.stat(72)),
                 ),
-                surround(${hint})
               )
             '';
           };
