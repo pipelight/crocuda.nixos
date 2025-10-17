7908e7e57f6baa704d22487818105d5899914d58
areskill <areskul@areskul.com>
2025-10-17 15:34:10 +0200

    update deps

--- virtualization/virshle.nix
+++ virtualization/virshle.nix
@@ -3,27 +3,25 @@
   pkgs,
   lib,
   ...
-}: let
-  cfg = config.crocuda;
-in
-  with lib;
-    mkIf config.crocuda.virtualization.virshle.enable {
-      # environment.etc = {
-      #   "virshle/config.toml".text = builtins.readFile ./dotfiles/virshle/config.toml;
-      # };
+}:
+with lib;
+  mkIf config.crocuda.virtualization.virshle.enable {
+    # environment.etc = {
+    #   "virshle/config.toml".text = builtins.readFile ./dotfiles/virshle/config.toml;
+    # };
 
-      services.virshle = {
-        enable = true;
-        logLevel = "debug";
-        user = "anon";
-        dhcp.defaultConfig = true;
-      };
+    services.virshle = {
+      enable = true;
+      logLevel = "debug";
+      user = "anon";
+      dhcp.defaultConfig = true;
+    };
 
-      environment.systemPackages = with pkgs; [
-        # Build images based on flakes and local config
-        nixos-generators
-        rqlite
-        disko
-        cdrkit
-      ];
-    }
+    environment.systemPackages = with pkgs; [
+      # Build images based on flakes and local config
+      nixos-generators
+      rqlite
+      disko
+      cdrkit
+    ];
+  }
