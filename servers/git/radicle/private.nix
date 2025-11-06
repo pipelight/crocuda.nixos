48b1d29c3698a0cf71e96a40c6b6f5552a574a70
areskill <areskul@areskul.com>
2025-11-06 17:12:06 +0100

    refactor(git): update radicle config to latest nix

--- servers/git/radicle/private.nix
+++ servers/git/radicle/private.nix
@@ -12,23 +12,29 @@ in
       services.radicle = {
         enable = true;
         checkConfig = true;
+
         privateKeyFile = "/etc/radicle/id_ed25519";
-        publicKey = "/etc/radicle/id_ed25519.pub";
+        # privateKeyFile = "/var/lib/radicle/keys/radicle";
+
+        # publicKey = "/etc/radicle/id_ed25519.pub";
+        # publicKey = "/var/lib/radicle/keys/radicle.pub";
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPUZIvo0RPhao7s73Wt613qJw/P+KseGEnqA7/gT/AYx";
+
         node = {
-          listenAddress = "::1";
+          listenAddress = "[::1]";
           listenPort = 8776;
         };
         httpd = {
           enable = true;
-          listenAddress = "::1";
+          listenAddress = "[::1]";
           listenPort = 8786;
         };
         settings = {
           "node" = {
             "alias" = "anon";
-            "externalAddresses" = ["seed.example.com:8776"];
-            "policy" = "allow";
-            "scope" = "all";
+            "externalAddresses" = [
+              (config.networking.hostName + ".lan" + ":8776")
+            ];
           };
         };
       };
